1ea8ea8b831c3e2b2bf5a82ae5a2ad32ce47f126
rajpal229 <rajpalrawat229@gmail.com>
2023-01-14 18:28:48 +0530

    Added: UC1_CreateDatabase

---
+++ CreateDatabaseClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AddressBookSql
+{
+    public class CreateDatabaseClass
+    {
+        public static void CreateDatabase()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=I-CHANGE-THE-NA\SQLEXPRESS;Initial catalog=master;Integrated Security=true");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("Create database AddressBook", connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Databass ctearted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBookSql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDatabaseClass.CreateDatabase();
+        }
+    }
+}
