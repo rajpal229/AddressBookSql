0864f291c32d3ddc4710c686c6d458de0629d4c0
rajpal229 <rajpalrawat229@gmail.com>
2023-01-14 19:15:09 +0530

    Added: UC3_Insert_Contacts

---
+++ InsertDataClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSql
+{
+    public class InsertDataClass
+    {
+        public static void InsertData(string FirstName, string LastName, string Address, string City, string State, int Zip, long PhoneNumber, string Email) 
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(@"Data Source=I-CHANGE-THE-NA\SQLEXPRESS;Initial catalog=AddressBook;Integrated Security=true");
+                connection.Open();
+                SqlCommand cmd = new SqlCommand($"insert into Address_Book values('{FirstName}','{LastName}','{Address}','{City}','{State}','{Zip}','{PhoneNumber}','{Email}')", connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Data Inserted");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Option\n1.Create Table (Address Book) ");
+            Console.WriteLine("Enter Option\n1.Create Table\n2.Insert Data Into Table");
             int op = Convert.ToInt32(Console.ReadLine());
             while (true)
             {
@@ -12,6 +12,25 @@
                 {
                     case 1:
                         InsertTableClass.InsertTable();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter FirstName");
+                        string FirstName = Console.ReadLine();
+                        Console.WriteLine("Enter LastName");
+                        string LastName = Console.ReadLine();
+                        Console.WriteLine("Enter Address");
+                        string Address = Console.ReadLine();
+                        Console.WriteLine("Enter City");
+                        string City = Console.ReadLine();
+                        Console.WriteLine("Enter State");
+                        string State = Console.ReadLine();
+                        Console.WriteLine("Enter Zip");
+                        int Zip = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter PhoneNumber");
+                        long PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Enter Email");
+                        string Email = Console.ReadLine();
+                        InsertDataClass.InsertData(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email);
                         break;
                 }
                 break;
